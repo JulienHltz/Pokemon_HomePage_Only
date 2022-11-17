3de9390bb81296ea5a74fa97e9606887de154bb0
Julien Overholtzer <47stilldreaming@gmail.com>
2022-11-17 18:45:29 +0100

    testing modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,27 +15,49 @@ let body = document.getElementById("body");
 let faSound = document.getElementById("fa_sound");
 
 window.onload = function () {
-  body.style.display = "flex";
-  let MyPrompt = prompt(
-    'Voulez-vous activer le son en jeu ? Répondre par "oui" ou par "non"'
-  );
+  // body.style.display = "flex";
+  // let MyPrompt = prompt(
+  //   'Voulez-vous activer le son en jeu ? Répondre par "oui" ou par "non"'
+  // );
 
-  if (MyPrompt === "oui") {
-    console.log("test musique");
-    alert("Lancement de la musique.");
-      audioEl.play();
-    refreshCSS();
-    faSound.classList.replace("fa-volume-xmark", "fa-volume-high");
-    soundBtn.style.backgroundColor = "#2f779c";
-  }
+//   if (MyPrompt === "oui") {
+//     console.log("test musique");
+//     alert("Lancement de la musique.");
+//     audioEl.play();
+//     refreshCSS();
+//     faSound.classList.replace("fa-volume-xmark", "fa-volume-high");
+//     soundBtn.style.backgroundColor = "#2f779c";
+//   }
 
-  if (MyPrompt === "non") {
-    alert("Lancement de la partie sans musique.");
-    refreshCSS();
-  }
-};
+//   if (MyPrompt === "non") {
+//     alert("Lancement de la partie sans musique.");
+//     refreshCSS();
+//   }
+// };
 
 //  Mute or UnMute sound with Btn
+let ouiBtn = document.getElementById('ouiBtn')
+let nonBtn = document.getElementById('nonBtn')
+let modal = document.getElementById('modalContainer')
+
+ouiBtn.addEventListener("click", () =>{
+  // alert("Activation du son en jeu");
+  modal.style.display="none"
+  refreshCSS();
+  audioEl.play();
+  faSound.classList.replace("fa-volume-xmark", "fa-volume-high");
+  soundBtn.style.backgroundColor = "#2f779c";
+})
+
+nonBtn.addEventListener("click", () =>{
+  alert("Le son en jeu est désormais désactivé");
+  modal.style.display="none"
+  refreshCSS();
+  faSound.classList.replace("fa-volume-high", "fa-volume-xmark");
+  soundBtn.style.backgroundColor = "#2f779c";
+})
+
+
 
 soundBtn.addEventListener("click", () => {
   // console.log(audioEl.paused);
@@ -51,3 +73,5 @@ soundBtn.addEventListener("click", () => {
 
   }
 });
+
+}
